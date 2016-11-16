60a7af695c1cdc32ba2ffa0aec469688387997e9
Nathan W <nate01776@gmail.com>
2016-11-16 15:10:02 -0500

    working through streak logic

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -21,9 +21,6 @@ class PicturesController < ApplicationController
       @picture.day_number = 1
       @user.first_day = Time.now
       @user.current_streak = 1
-    elsif @user.pictures.last.created_at != (Time.now - 1.day)
-      @picture.day_number = @user.pictures.length + 1
-      @user.current_streak = 1
     else
       @picture.day_number = @user.pictures.length + 1
       @user.current_streak += 1
